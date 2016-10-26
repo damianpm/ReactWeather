57d2a8e73f6c79e29914dc80d183002bb0554cef
Damian Peralta <peralta.damian@yahoo.com.mx>
2016-10-26 17:07:16 -0500

    Fix modal error

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,21 +1,20 @@
 var axios = require('axios');
 
-const OPEN_WEATER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=e7bed8929565b0bf08de2bf0c6825dca';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 module.exports = {
-    getTemp: function(location) {
-        var encodedeLoction = encodeURIComponent(location);
-        var requestUrl = `${OPEN_WEATER_MAP_URL}&q=${encodedeLoction}`;
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-        return axios.get(requestUrl).then(function(res) {
-          console.log(res);
-          if (res.data.cod && res.data.message){
-            throw new Error(res.data.message);
-          }else{
-            return res.data.main.temp;
-          }
-        }, function(res) {
-          throw new Error(res.data.message);
-        });
-    }
-};
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
